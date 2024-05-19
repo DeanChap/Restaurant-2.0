73cbd7817056f83025115372ddee1a5df4298af6
Dean Chap <cchapotarong@mun.ca>
2024-05-18 20:56:25 -0600

    navbar commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
       <div className='app__navbar-login'>
         <a href="#login" className='p__opensans'>Log In / Register</a>
-        <div />
+    
         <a href="/" className='p__opensans'>Book Table</a>
       </div>
 
